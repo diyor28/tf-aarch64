5087bdd3895899aa8cd73fc3cd7916fa68c51048
Diyor Khaydarov <diyor28july@gmail.com>
2022-10-22 14:05:22 +0500

    nginx server

---
+++ ui/back/main.py
@@ -0,0 +1,22 @@
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+WHEELS_DIR = os.getenv("WHEELS_DIR")
+templates = Jinja2Templates(directory="templates")
+
+
+def wheels_list() -> list[dict[str, str]]:
+    wheel_files = [f for f in os.listdir(WHEELS_DIR) if os.path.isfile(os.path.join(WHEELS_DIR, f))]
+    res = []
+    for file in wheel_files:
+        res.append({"name": file, "url": f"/builds/{file}"})
+    return res
+
+
+@app.get("/wheels")
+def read_wheels(request: Request):
+    wheels = wheels_list()
+    return templates.TemplateResponse("wheels.html", {"request": request, "wheels": wheels})
